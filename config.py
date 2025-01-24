fc66359d43fe8a685aa20525f9e4125da1fc2d42
Nikolaos Plastiras <nipla31@gmail.com>
2025-01-24 10:12:21 +0100

    Added a new plotting script that uses the cmsstyle module

--- config.py
+++ config.py
@@ -25,22 +25,22 @@ class Config:
     self.vars = [
       [self.el_prefix + "pt","el_pt","Electron p_{T} [GeV]", 100, 0, 200],
       [self.mu_prefix + "pt","mu_pt","Muon p_{T} [GeV]", 100, 0, 200],
-      # [self.Z_prefix + "mass","Zcandidate_mass","Z candidate mass [GeV]", 100, 0, 150],
-      # [self.Z_prefix + "pt","Zcandidate_pt","Z candidate p_{T} [GeV]", 100, 0, 200],
-      # [self.Z_prefix + "eta","Zcandidate_eta","Z candidate #eta", 40, -8, 8],
-      # [self.Z_prefix + "phi","Zcandidate_phi","Z candidate #phi [rad]", 40, -4, 4],
-      # [self.Z_prefix + "onshell_mass","Zcandidate_onshell_mass","on-shell Z candidate mass [GeV]", 100, 0, 120],
-      # [self.Z_prefix + "offshell_mass","Zcandidate_offshell_mass","off-shell Z candidate mass [GeV]", 60, 0, 120],
-      # [self.H_prefix + "mass","Hcandidate_mass","H candidate mass [GeV]", 100, 0, 150],
-      # [self.H_prefix + "pt","Hcandidate_pt","H candidate p_{T} [GeV]", 100, 0, 200],
-      # [self.H_prefix + "eta","Hcandidate_eta","H candidate #eta", 40, -8, 8],
-      # [self.H_prefix + "phi","Hcandidate_phi","H candidate #phi [rad]", 40, -4, 4],
-      # [self.jet_prefix + "mass[0]","ak4_mass_0","leading jet mass [GeV]", 100, 0, 200],
-      # [self.jet_prefix + "pt[0]","ak4_pt_0","leading jet p_{T} [GeV]", 100, 0, 200],
-      # [self.jet_prefix + "eta[0]","ak4_eta_0","leading jet #eta", 80, -8, 8],
-      # [self.jet_prefix + "cvbdisc[0]","ak4_cvbdisc_0","leading jet c vs b score", 50, 0, 1],
-      # [self.jet_prefix + "cvldisc[0]","ak4_cvldisc_0","leading jet c v l score", 50, 0, 1],      
-      # [self.jet_prefix + "bdisc[0]","ak4_bdisc_0","leading jet b score", 50, 0, 1],
+      [self.Z_prefix + "mass","Zcandidate_mass","Z candidate mass [GeV]", 100, 0, 150],
+      [self.Z_prefix + "pt","Zcandidate_pt","Z candidate p_{T} [GeV]", 100, 0, 200],
+      [self.Z_prefix + "eta","Zcandidate_eta","Z candidate #eta", 40, -8, 8],
+      [self.Z_prefix + "phi","Zcandidate_phi","Z candidate #phi [rad]", 40, -4, 4],
+      [self.Z_prefix + "onshell_mass","Zcandidate_onshell_mass","on-shell Z candidate mass [GeV]", 100, 0, 120],
+      [self.Z_prefix + "offshell_mass","Zcandidate_offshell_mass","off-shell Z candidate mass [GeV]", 60, 0, 120],
+      [self.H_prefix + "mass","Hcandidate_mass","H candidate mass [GeV]", 100, 0, 150],
+      [self.H_prefix + "pt","Hcandidate_pt","H candidate p_{T} [GeV]", 100, 0, 200],
+      [self.H_prefix + "eta","Hcandidate_eta","H candidate #eta", 40, -8, 8],
+      [self.H_prefix + "phi","Hcandidate_phi","H candidate #phi [rad]", 40, -4, 4],
+      [self.jet_prefix + "mass[0]","ak4_mass_0","leading jet mass [GeV]", 100, 0, 200],
+      [self.jet_prefix + "pt[0]","ak4_pt_0","leading jet p_{T} [GeV]", 100, 0, 200],
+      [self.jet_prefix + "eta[0]","ak4_eta_0","leading jet #eta", 80, -8, 8],
+      [self.jet_prefix + "cvbdisc[0]","ak4_cvbdisc_0","leading jet c vs b score", 50, 0, 1],
+      [self.jet_prefix + "cvldisc[0]","ak4_cvldisc_0","leading jet c v l score", 50, 0, 1],      
+      [self.jet_prefix + "bdisc[0]","ak4_bdisc_0","leading jet b score", 50, 0, 1],
       # ["deltaR(lep1_eta, lep2_eta, lep1_phi, lep2_phi)","dR_lep1_lep_2","#DeltaR(lep1,lep2)", 60, 0, 6],
       # ["deltaR(lep1_eta, lep3_eta, lep1_phi, lep3_phi)","dR_lep1_lep_3","#DeltaR(lep1,lep3)", 60, 0, 6],
       # ["deltaR(lep1_eta, lep4_eta, lep1_phi, lep4_phi)","dR_lep1_lep_4","#DeltaR(lep1,lep4)", 60, 0, 6],
@@ -49,11 +49,11 @@ class Config:
       # ["deltaR(lep3_eta, lep4_eta, lep3_phi, lep4_phi)","dR_lep3_lep_4","#DeltaR(lep3,lep4)", 60, 0, 6],
       ]
     
-    self.output_plots_dir = "plots/"
-    self.base_dir = "/eos/user/i/iparaske/HcTrees/mc/2018/merged/"
+    self.output_plots_dir = "plots/2021/"
+    self.base_dir = "/eos/user/n/nplastir/H+c/trees/mc/2021/merged"
     self.cuts = "1" # if you don't want cuts remember to put "1"
     self.plot_format = "png"
-    self.dataset_legend = "59.8 fb^{-1} (13 TeV)"
+    self.dataset_legend = "" #"38.01"
     self.stack_ymin = 1
     self.stack_ymax = 5e6
     self.set_logy = False 
@@ -61,27 +61,36 @@ class Config:
     
     ## plot legend
     self.legend = {
-      "DYJets":"DYJets",
       "ggH":"ggH",
       "VBF":"VBF",
-      "bbH":"bbH",
-      "Hc":"Hc",
-      "tqH":"tqH",
+      "WplusH":"WplusH",
+      "WminusH":"WminusH",
+      "ZH":"ZH",
       "ttH":"ttH",
-      "VH":"VH",
+      "bbH":"bbH",
       "ZZ": "ZZ",
+      "WWZ": "WWZ",
+      "WZZ": "WZZ",
+      "ZZZ": "ZZZ",
+      "TTWW": "TTWW",
+      "TTZZ": "TTZZ",
+      "WZ": "WZ",
+      "DYJets":"DYJets",
+      "TTto2L2Nu":"TTto2L2Nu",
+      # "Hc":"Hc",
     }
     
     ## plot colors
     self.colors = {
       "DYJets": 829,
-      "ggH": 38,
-      "VBF": 43,
-      "bbH": 821,
-      "Hc": 880,
-      "tqH": 829,
-      "ttH": 811,
-      "VH": 859,
+      # "ggH": 38,
+      # "VBF": 43,
+      # "bbH": 821,
+      # "Hc": 880,
+      # "tqH": 829,
+      # "ttH": 811,
+      # "VH": 859,
+      "WZ": 859,
       "ZZ": 623,
     }  
     
---
+++ makePlotsCMS.py
@@ -0,0 +1,122 @@
+import ROOT
+import argparse
+import os
+import config
+import time
+import utils
+import cmsstyle as CMS  # Import cmsstyle
+
+ROOT.ROOT.EnableImplicitMT()  # Enable multi-threading for RDataFrame
+ROOT.gInterpreter.ProcessLine('#include "cpp_functions.C"')
+ROOT.gROOT.SetBatch(True)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--type', type=str, help='Type of plots', choices=['stack', 'shape'], default='stack')
+args = parser.parse_args()
+
+def create_RDF(filename):
+    print(f"Creating RDF for sample {filename}")
+    filename_path = os.path.join(config_file.base_dir, filename)
+    df = ROOT.RDataFrame("Events", filename_path)
+    return df
+
+def create_plots(config_file):
+    samples_dict = config_file.samples_dict
+    variables = config_file.vars
+
+    samples_filenames = config_file.get_samples_filenames()
+    RDF_dict = {filename: create_RDF(filename) for filename in samples_filenames}
+
+    # CMS Styling Settings
+    CMS.SetExtraText("Preliminary")
+    CMS.SetLumi(config_file.dataset_legend)
+    CMS.SetEnergy("13.6")
+    CMS.ResetAdditionalInfo()
+
+    for variable in variables:
+        print(f"Plotting var {variable[0]}")
+        histos_dict = {}
+
+        for sample in samples_dict.keys():
+            df = RDF_dict[samples_dict[sample][0]]
+            hist = df.Filter(config_file.cuts).Define("plotvar_", variable[0]).Histo1D(
+                (f"hist_{sample}_{variable[0]}", "", variable[3], variable[4], variable[5]), "plotvar_"
+            )
+            hist = utils.add_underflow(hist)
+            hist = utils.add_overflow(hist)
+            if args.type == "stack":
+                hist.SetLineColor(ROOT.kBlack)
+            if args.type == "shape":
+                # hist.SetLineColor(config_file.colors[sample])
+                hist.Scale(1 / hist.Integral())
+            histos_dict[sample] = hist.GetPtr()
+
+        # CMSStyle Canvas and Legend
+        canv_name = f"{variable[1]}_canvas"
+        y_title = "Events" if args.type == "stack" else "Normalized to 1"
+        canvas = CMS.cmsCanvas(canv_name, variable[4], variable[5], 0, 1, variable[2], y_title, square=CMS.kSquare, extraSpace=0.05, iPos=0)
+
+        stack = ROOT.THStack("stack", f";{variable[2]};{y_title}")
+        stack2 = ROOT.THStack("stack2", f";{variable[2]};{y_title}")
+        legend = CMS.cmsLeg(0.72, 0.45, 0.92, 0.88, textSize=0.035, columns=1)
+        
+        for sample, hist in histos_dict.items():
+            stack2.Add(hist)
+
+        # Adjust y-axis offsets and log scaling
+        y_max = 1
+        y_min = 0.0
+        if config_file.set_logy:
+            canvas.SetLogy()
+            y_max = 10 * CMS.cmsReturnMaxY(stack2)
+            y_min = 1.0
+        elif config_file.set_logy and args.type == "stack": 
+            y_max = 1.5 * CMS.cmsReturnMaxY(stack2)
+        else:
+            y_max = 1.2* CMS.cmsReturnMaxY(stack2)
+
+        # Set the CMS canvas y-axis
+        CMS.GetcmsCanvasHist(canvas).GetYaxis().SetRangeUser(y_min, y_max)
+        CMS.GetcmsCanvasHist(canvas).GetYaxis().SetTitleOffset(1.6)
+        # Scientific notation
+        hdf = CMS.GetcmsCanvasHist(canvas)
+        hdf.GetYaxis().SetMaxDigits(2)
+        # Shift multiplier position
+        ROOT.TGaxis.SetExponentOffset(-0.10, 0.01, "Y")
+
+        CMS.cmsDrawStack(stack, legend, histos_dict)
+
+        # Save the canvas
+        canvas.SaveAs(os.path.join(config_file.output_plots_dir, args.type, f"{variable[1]}." + config_file.plot_format))
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    config_file = config.Config()
+    os.makedirs(os.path.join(config_file.output_plots_dir, args.type), exist_ok=True)
+
+    # Samples will be stacked in this order
+    config_file.add_sample(name="ggH", root_file="ggH_tree.root",cuts=1)
+    config_file.add_sample(name="VBF", root_file="VBF_tree.root",cuts=1)
+    config_file.add_sample(name="WplusH", root_file="WplusH_tree.root",cuts=1)
+    config_file.add_sample(name="WminusH", root_file="WminusH_tree.root",cuts=1)
+    config_file.add_sample(name="ZH", root_file="ZH_tree.root",cuts=1)
+    config_file.add_sample(name="ttH", root_file="ttH_tree.root",cuts=1)
+    config_file.add_sample(name="bbH", root_file="bbH_tree.root",cuts=1)
+    config_file.add_sample(name="ZZ", root_file="ZZ_tree.root",cuts=1)
+    config_file.add_sample(name="WWZ", root_file="WWZ_tree.root",cuts=1)
+    config_file.add_sample(name="WZZ", root_file="WZZ_tree.root",cuts=1)
+    config_file.add_sample(name="ZZZ", root_file="ZZZ_tree.root",cuts=1)
+    config_file.add_sample(name="TTWW", root_file="TTWW_tree.root",cuts=1)
+    config_file.add_sample(name="TTZZ", root_file="TTZZ_tree.root",cuts=1)
+    config_file.add_sample(name="WZ", root_file="WZ_tree.root",cuts=1)
+    config_file.add_sample(name="DYJets", root_file="DYJets_tree.root",cuts=1)
+    config_file.add_sample(name="TTto2L2Nu", root_file="TTto2L2Nu_tree.root",cuts=1)
+    # config_file.add_sample(name="Hc", root_file="Hc_tree.root",cuts=1)
+ 
+
+    create_plots(config_file)
+
+    end_time = time.time()
+    print(f"Elapsed time: {(end_time - start_time)/60:.2f} minutes")
