1ac7941e602cbdbdc1e68bf8369bfd011da14cb7
Nikolaos Plastiras <nipla31@gmail.com>
2025-04-02 10:13:59 +0200

    CR plots

--- config.py
+++ config.py
@@ -26,58 +26,39 @@ class Config:
     self.ZZ4e_prefix="ZZ4e_"
     self.ZZ4mu_prefix="ZZ4mu_"
     self.ZZ2e2mu_prefix="ZZ2e2mu_"
+
+    self.ZLall_prefix = "ZLall_"
+    self.ZLalle_prefix = "ZLalle_"
+    self.ZLallmu_prefix = "ZLallmu_"
+    self.ZLpass_prefix = "ZLpass_"
+    self.ZLpasse_prefix = "ZLpasse_"
+    self.ZLpassmu_prefix = "ZLpassmu_"
       
     ## [branch name, plot name, x-axis label, nbins, xlow, xhigh]
     self.vars = [
       # [self.el_prefix + "pt","el_pt","Electron p_{T} [GeV]", 100, 0, 200],
       # [self.mu_prefix + "pt","mu_pt","Muon p_{T} [GeV]", 100, 0, 200],
-      # [self.Z_prefix + "mass","Zcandidate_mass","Z candidate mass [GeV]", 100, 0, 150],
-      # [self.Z_prefix + "pt","Zcandidate_pt","Z candidate p_{T} [GeV]", 100, 0, 200],
-      # [self.Z_prefix + "eta","Zcandidate_eta","Z candidate #eta", 40, -8, 8],
-      # [self.Z_prefix + "phi","Zcandidate_phi","Z candidate #phi [rad]", 40, -4, 4],
-      # [self.Z_prefix + "onshell_mass","Zcandidate_onshell_mass","on-shell Z candidate mass [GeV]", 100, 0, 120],
-      # [self.Z_prefix + "offshell_mass","Zcandidate_offshell_mass","off-shell Z candidate mass [GeV]", 60, 0, 120],
-      [self.H_prefix + "mass","Hcandidate_mass","H candidate mass [GeV]", 56, 70, 350],
-      [self.H4e_prefix + "mass","Hcandidate_mass_4e","4e mass [GeV]", 56, 70, 350],
-      [self.H4mu_prefix + "mass","Hcandidate_mass_4mu","4#mu mass [GeV]", 56, 70, 350],
-      [self.H2e2mu_prefix + "mass","Hcandidate_mass_2e2mu","2e2#mu mass [GeV]", 56, 70, 350],
-      # [self.H_prefix + "mass_4mu","Hcandidate_mass_4mu","4#mu mass [GeV]", 56, 70, 350],
-      # [self.H_prefix + "mass_4e","Hcandidate_mass_4e","4e mass [GeV]", 56, 70, 350],
-      # [self.H_prefix + "mass_2e2mu","Hcandidate_mass_2e2mu","2e2#mu mass [GeV]", 56, 70, 350],
-      # [self.H_prefix + "pt","Hcandidate_pt","H candidate p_{T} [GeV]", 100, 0, 200],
-      # [self.H_prefix + "eta","Hcandidate_eta","H candidate #eta", 40, -8, 8],
-      # [self.H_prefix + "phi","Hcandidate_phi","H candidate #phi [rad]", 40, -4, 4],
-      [self.ZZ_prefix + "mass","ZZcandidate_mass","ZZ candidate mass [GeV]", 56, 70, 350],
-      [self.ZZ4e_prefix + "mass","ZZcandidate_mass_4e","4e mass [GeV]", 56, 70, 350],
-      [self.ZZ4mu_prefix + "mass","ZZcandidate_mass_4mu","4#mu mass [GeV]", 56, 70, 350],
-      [self.ZZ2e2mu_prefix + "mass","ZZcandidate_mass_2e2mu","2e2#mu mass  [GeV]", 56, 70, 350],
-      # [self.ZZ_prefix + "mass_4mu","ZZcandidate_mass_4mu","4#mu mass [GeV]", 56, 70, 350],
-      # [self.ZZ_prefix + "mass_4e","ZZcandidate_mass_4e","4e mass [GeV]", 56, 70, 350],
-      # [self.ZZ_prefix + "mass_2e2mu","ZZcandidate_mass_2e2mu","2e2#mu mass [GeV]", 56, 70, 350],
-      # [self.ZZ_prefix + "pt","ZZcandidate_pt","ZZ candidate p_{T} [GeV]", 100, 0, 200],
-      # [self.ZZ_prefix + "eta","ZZcandidate_eta","ZZ candidate #eta", 40, -8, 8],
-      # [self.ZZ_prefix + "phi","ZZcandidate_phi","ZZ candidate #phi [rad]", 40, -4, 4],
-      # [self.jet_prefix + "mass[0]","ak4_mass_0","leading jet mass [GeV]", 100, 0, 200],
-      # [self.jet_prefix + "pt[0]","ak4_pt_0","leading jet p_{T} [GeV]", 100, 0, 200],
-      # [self.jet_prefix + "eta[0]","ak4_eta_0","leading jet #eta", 80, -8, 8],
-      # [self.jet_prefix + "cvbdisc[0]","ak4_cvbdisc_0","leading jet c vs b score", 50, 0, 1],
-      # [self.jet_prefix + "cvldisc[0]","ak4_cvldisc_0","leading jet c v l score", 50, 0, 1],      
-      # [self.jet_prefix + "bdisc[0]","ak4_bdisc_0","leading jet b score", 50, 0, 1],
-      # ["deltaR(lep1_eta, lep2_eta, lep1_phi, lep2_phi)","dR_lep1_lep_2","#DeltaR(lep1,lep2)", 60, 0, 6],
-      # ["deltaR(lep1_eta, lep3_eta, lep1_phi, lep3_phi)","dR_lep1_lep_3","#DeltaR(lep1,lep3)", 60, 0, 6],
-      # ["deltaR(lep1_eta, lep4_eta, lep1_phi, lep4_phi)","dR_lep1_lep_4","#DeltaR(lep1,lep4)", 60, 0, 6],
-      # ["deltaR(lep2_eta, lep3_eta, lep2_phi, lep3_phi)","dR_lep2_lep_3","#DeltaR(lep2,lep3)", 60, 0, 6],
-      # ["deltaR(lep2_eta, lep4_eta, lep2_phi, lep4_phi)","dR_lep2_lep_4","#DeltaR(lep2,lep4)", 60, 0, 6],
-      # ["deltaR(lep3_eta, lep4_eta, lep3_phi, lep4_phi)","dR_lep3_lep_4","#DeltaR(lep3,lep4)", 60, 0, 6],
+      [self.ZLall_prefix + "pt2", "l_pt", "Extra lepton p_{T} [GeV]", 100, 0, 200],
+      [self.ZLalle_prefix + "pt2", "e_pt", "Extra electron p_{T} [GeV]", 100, 0, 200],
+      [self.ZLallmu_prefix + "pt2", "mu_pt", "Extra muon p_{T} [GeV]", 100, 0, 200],
+      [self.ZLall_prefix + "eta2", "l_eta", "Extra lepton #eta [GeV]", 100, -3, 3],
+      [self.ZLalle_prefix + "eta2", "e_eta", "Extra electron #eta [GeV]", 100, -3, 3],
+      [self.ZLallmu_prefix + "eta2", "mu_eta", "Extra muon #eta [GeV]", 100, -3, 3],
+      [self.ZLpass_prefix + "pt2", "l_pass_pt", "Extra lepton passed p_{T} [GeV]", 100, 0, 200],
+      [self.ZLpasse_prefix + "pt2", "e_pass_pt", "Extra electron passed p_{T} [GeV]", 100, 0, 200],
+      [self.ZLpassmu_prefix + "pt2", "mu_pass_pt", "Extra muon passed p_{T} [GeV]", 100, 0, 200],
+      [self.ZLpass_prefix + "eta2", "l_pass_eta", "Extra lepton passed #eta [GeV]", 100, -3, 3],
+      [self.ZLpasse_prefix + "eta2", "e_pass_eta", "Extra electron passed #eta [GeV]", 100, -3, 3],
+      [self.ZLpassmu_prefix + "eta2", "mu_pass_eta", "Extra muon passed #eta [GeV]", 100, -3, 3],
       ]
     
-    self.output_plots_dir = "plots/trees_24_02/test"
-    self.base_dir = "/eos/user/n/nplastir/H+c/trees_24_02/mc/2022_2023_Combined"
+    self.output_plots_dir = "plots/trees_01_04/"
+    self.base_dir = "/eos/user/n/nplastir/H+c/trees_01_04/mc/2022/merged"
     self.cuts = "1" # if you don't want cuts remember to put "1"
     self.weights =  "genWeight * xsecWeight * lumiwgt * puWeight * muEffWeight * elEffWeight" #"LHEScaleWeightNorm * LHEPdfWeightNorm * PSWeightNorm"
     self.plot_format = "png"
     self.energy = "13.6"
-    self.dataset_legend = "61.95"
+    self.dataset_legend = "7.98"
 #     lumi_dict = {
 #     "2016APV": 19.52,
 #     "2016": 16.81,
